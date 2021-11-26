6181893bbaa8042fda7d779ba523081a148201a3
loczylevi <71691777+loczylevi@users.noreply.github.com>
2021-11-26 15:31:39 +0100

    Create programozasi_tetelek.py

---
+++ programozasi_tetelek.py
@@ -0,0 +1,23 @@
+def nagy(lista):
+    legnagyobb = 0
+    for szam in lista:
+        if legnagyobb < szam:
+            legnagyobb = szam
+    return legnagyobb
+
+
+def kicsi(lista):
+    legkisebb = lista[0]
+    for szam in lista:
+        if legkisebb > szam:
+            legkisebb = szam
+    return legkisebb
+
+def ossz(lista):
+    x = 0
+    for szam in lista:
+        ossz = x + szam
+        
+    return ossz
+
+        
