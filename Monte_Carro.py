5d1639961e8edee41237a1176abf70c7930b8857
loczylevi <71691777+loczylevi@users.noreply.github.com>
2022-01-10 21:13:31 +0100

    Create Monte_Carro.py

---
+++ Monte_Carro.py
@@ -0,0 +1,24 @@
+import math
+from math import sqrt
+import random
+import turtle
+wn = turtle.Screen()
+alex = turtle.Turtle()
+alex.penup()
+alex.shape("circle")
+alex.speed(100)
+szamlalo = 0
+for szam in range(100000):
+    vx = random.randint(-400,400)
+    vy = random.randint(-400,400)
+    alex.goto(vx,vy)
+    d = math.sqrt(vy**2 + vx**2)
+    if d > 400:
+        alex.color("blue")
+        alex.stamp()
+    else:
+        alex.color("red")
+        szamlalo = szamlalo + 1
+        alex.stamp()
+        
+pi = 4 * szamlalo / szam
